fc31244576813f1ab442ba260263f304832a0f05
rzvdaniel <rzvdaniel@gmail.com>
2019-06-25 22:16:02 +0100

    Updated apps fields

--- eCameleon.API/services/apps.service.js
+++ eCameleon.API/services/apps.service.js
@@ -9,67 +9,28 @@ module.exports = {
 	],
 
 	settings: {
-		fields: {
-			id: { type: "string", readonly: true, primaryKey: true, secure: true, columnName: "_id" },
-			name: { type: "string", maxlength: 250, required: true },
-			description: { type: "string", maxlength: 5000, required: true },
-			active: { type: "boolean", default: true },
-			protected: { type: "boolean", default: true },
-			createdAt: { type: "number", updateable: false, default: Date.now },
-			updatedAt: { type: "number", readonly: true, updateDefault: Date.now }
+		fields: ["_id", "name", "title", "description", "active", "created"]
+	},
+
+	hooks: {
+		before: {
+		  create(ctx) {
+			ctx.params.created = new Date();
+		  },
+		  update(ctx) {
+			ctx.params.updated = new Date();
+		  }
 		}
 	},
 
 	actions: {
 
-		list: {
-			rest: "GET /",
-			handler(ctx) {
-				return 'list';
-			}
-		},
+	},
 
-		get: {
-			rest: "GET /:id",
-			handler(ctx) {
-				return 'get';
-			}
-		},
+	/**
+	 * Methods
+	 */
+	methods: {
 
-		create: {
-			rest: "POST /",
-			params: {
-				name: { type: "string", maxlength: 250 },
-				description: { type: "string", maxlength: 5000, optional: true }
-			},
-			async handler(ctx) {
-				const entity = {
-					name:  ctx.params.name,
-					description: ctx.params.description,
-					active : true,
-					protected: true,
-					createdAt: Date.now(),
-					updatedAt: Date.now()
-				};
-
-				const app = await this.adapter.insert(entity);
-
-				return this.transformDocuments(ctx, {}, app);
-			}
-		},
-
-		update: {
-			rest: "PUT /:id",
-			handler(ctx) {
-				return 'update';
-			}
-		},
-
-		remove: {
-			rest: "DELETE /:id",
-			handler(ctx) {
-				return 'remove';
-			}
-		}
 	}
 };
